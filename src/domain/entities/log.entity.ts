6f49562ea3c71d8b76dc9ba1e581f224e4ff7b6f
Abel Aron Amieva <abelamieva@gmail.com>
2024-01-04 22:49:58 -0300

    se refactorizo el codigo para poder guardar el origen del error

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -5,19 +5,33 @@ export enum LogSeverityLevel {
     high = 'high'
 };
 
+export interface LogEntityOptions {
+    level: LogSeverityLevel;
+    message: string;
+    origin: string;
+    createdAt?: Date;
+}
+
 export class LogEntity {
     public level: LogSeverityLevel;
     public message: string;
     public createdAt: Date;
-    constructor(message: string, level: LogSeverityLevel) {
+    public origin: string;
+    constructor(options: LogEntityOptions) {
+        const { level, message, origin, createdAt = new Date() } = options;
         this.message = message;
         this.level = level;
-        this.createdAt = new Date();
+        this.createdAt = createdAt;
+        this.origin = origin;
     }
     static fromJson = (json: string): LogEntity => {
-        const { message, level, createdAt } = JSON.parse(json);
-        const log = new LogEntity(message, level);
-        log.createdAt = new Date(createdAt);
+        const { message, level, createdAt, origin } = JSON.parse(json);
+        const log = new LogEntity({
+            message,
+            level,
+            origin,
+            createdAt
+        });
         return log;
     }
 }
